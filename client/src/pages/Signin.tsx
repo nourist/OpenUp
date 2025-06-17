2aa1c44422dcb61be2b955971826eaad268bb489
nourist <hodinhvys@gmail.com>
2025-06-17 14:25:48 +0700

    feat(client): use react-hook-form for better performance and code quality

--- client/src/pages/Signin.tsx
+++ client/src/pages/Signin.tsx
@@ -1,26 +1,40 @@
 import { useTranslation } from 'react-i18next';
-import { useState } from 'react';
 import { Link, useNavigate } from 'react-router';
 import { toast } from 'react-toastify';
 import { motion } from 'framer-motion';
 import { CircleAlert } from 'lucide-react';
+import { useForm } from 'react-hook-form';
 
 import GoogleSigninButton from '~/components/GoogleSigninButton';
 import AbstractGradientShape from '~/components/AbstractGradientShape';
 import { signin } from '~/services/auth';
 import useAuthStore from '~/stores/authStore';
+import Logo from '~/assets/logo.svg';
 
 const Signin = () => {
 	const { t } = useTranslation('auth');
 	const navigate = useNavigate();
 	const { setUser } = useAuthStore();
 
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isSubmitting },
+	} = useForm({
+		defaultValues: {
+			email: '',
+			password: '',
+		},
+	});
 
-	const [fieldErrors, setFieldErrors] = useState<Partial<Record<'password' | 'email', string | null | undefined>>>({});
-
-	const [signingIn, setSigningIn] = useState(false);
+	const onSubmit = (data: any) =>
+		signin(data.email, data.password)
+			.then((res) => {
+				toast.success(res.message);
+				setUser(res.user);
+				navigate('/app');
+			})
+			.catch(toast.error);
 
 	return (
 		<div className="bg-base-200 flex h-[100vh] items-center justify-center p-8">
@@ -31,23 +45,29 @@ const Signin = () => {
 				className="bg-base-100 flex aspect-video h-[calc(100%-130px)] max-w-[calc(100%-64px)] rounded-4xl shadow-lg"
 			>
 				<AbstractGradientShape className="hidden w-1/2 rounded-tl-4xl md:block" />
-				<div className="w-full p-10 md:w-1/2">
-					<h1 className="font mb-10 text-3xl">{t('sign-in').capitalize()}</h1>
+				<form onSubmit={handleSubmit(onSubmit)} className="w-full p-10 md:w-1/2">
+					<h1 className="font mb-10 text-3xl flex items-center gap-2">
+						<Link to="/">
+							<Logo />
+						</Link>
+						{t('sign-in').capitalize()}
+					</h1>
 
 					<label htmlFor="email" className="text-sm font-semibold capitalize">
 						{t('email')}
 					</label>
 					<input
 						id="email"
-						value={email}
-						onChange={(e) => setEmail(e.target.value)}
-						onFocus={() => setFieldErrors((prev) => ({ ...prev, email: null }))}
-						className={`input mt-1 placeholder:capitalize ${fieldErrors.email && 'text-error'}`}
+						{...register('email', {
+							required: { value: true, message: 'email-required-msg' },
+							pattern: { value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, message: 'invalid-email-msg' },
+						})}
+						className={`input mt-1 placeholder:capitalize ${errors.email && 'text-error'}`}
 						placeholder={t('email')}
 					/>
-					<div data-hide={!fieldErrors.email} className="text-xs text-error mb-1 mt-0.5 flex items-center gap-1 font-medium h-4 data-[hide=true]:opacity-0">
+					<div data-hide={!errors.email} className="text-xs text-error mb-1 mt-0.5 flex items-center gap-1 font-medium h-4 data-[hide=true]:opacity-0">
 						<CircleAlert size={12} strokeWidth={2.5} />
-						{fieldErrors.email?.capitalize()}
+						{errors.email?.message && t(errors.email?.message).capitalize()}
 					</div>
 
 					<label htmlFor="password" className="text-sm font-semibold capitalize">
@@ -56,55 +76,18 @@ const Signin = () => {
 					<input
 						id="password"
 						type="password"
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-						onFocus={() => setFieldErrors((prev) => ({ ...prev, password: null }))}
-						className={`input mt-1 placeholder:capitalize ${fieldErrors.password && 'text-error'}`}
+						{...register('password', { required: { value: true, message: 'password-required-msg' } })}
+						className={`input mt-1 placeholder:capitalize ${errors.password && 'text-error'}`}
 						placeholder={t('password')}
 					/>
-					<div data-hide={!fieldErrors.password} className="text-xs text-error mb-1 mt-0.5 flex items-center gap-1 font-medium h-4 data-[hide=true]:opacity-0">
+					<div data-hide={!errors.password} className="text-xs text-error mb-1 mt-0.5 flex items-center gap-1 font-medium h-4 data-[hide=true]:opacity-0">
 						<CircleAlert size={12} strokeWidth={2.5} />
-						{fieldErrors.password?.capitalize()}
+						{errors.password?.message && t(errors.password?.message).capitalize()}
 					</div>
 
 					<button
-						disabled={signingIn}
-						onClick={() => {
-							let ok = true;
-							if (!email) {
-								setFieldErrors((prev) => ({
-									...prev,
-									email: 'email should not be empty',
-								}));
-								ok = false;
-							}
-							if (!password) {
-								setFieldErrors((prev) => ({
-									...prev,
-									password: 'password should not be empty',
-								}));
-								ok = false;
-							}
-							const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-							const isValidEmail = (email: string) => emailRegex.test(email);
-							if (!isValidEmail(email)) {
-								setFieldErrors((prev) => ({
-									...prev,
-									email: 'email is not valid',
-								}));
-								ok = false;
-							}
-							if (!ok) return;
-							setSigningIn(true);
-							signin(email, password)
-								.then((res) => {
-									toast.success(res.message);
-									setUser(res.user);
-									navigate('/app');
-								})
-								.catch(toast.error)
-								.finally(() => setSigningIn(false));
-						}}
+						disabled={isSubmitting}
+						type="submit"
 						className="from-primary to-secondary mt-8 h-10 w-full rounded-full bg-gradient-to-r font-bold text-neutral-content capitalize hover:shadow-lg"
 					>
 						{t('sign-in')}
@@ -116,7 +99,7 @@ const Signin = () => {
 						</Link>
 					</div>
 					<GoogleSigninButton />
-				</div>
+				</form>
 			</motion.div>
 		</div>
 	);
--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,27 +1,41 @@
 import { useTranslation } from 'react-i18next';
-import { useState } from 'react';
 import { Link, useNavigate } from 'react-router';
 import { toast } from 'react-toastify';
 import { motion } from 'framer-motion';
 import { CircleAlert } from 'lucide-react';
+import { useForm } from 'react-hook-form';
 
 import useAuthStore from '~/stores/authStore';
 import { signup } from '~/services/auth';
 import AbstractGradientShape from '~/components/AbstractGradientShape';
 import Tooltip from '~/components/Tooltip';
+import Logo from '~/assets/logo.svg';
 
 const Signup = () => {
 	const { t } = useTranslation('auth');
 	const navigate = useNavigate();
 	const { setUser } = useAuthStore();
 
-	const [displayName, setDisplayName] = useState('');
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isSubmitting },
+	} = useForm({
+		defaultValues: {
+			email: '',
+			password: '',
+			name: '',
+		},
+	});
 
-	const [fieldErrors, setFieldErrors] = useState<Partial<Record<'name' | 'email' | 'password', string | null | undefined>>>({});
-
-	const [signingUp, setSigningUp] = useState(false);
+	const onSubmit = (data: any) =>
+		signup(data.email, data.password, data.name)
+			.then((res) => {
+				toast.success(res.message);
+				setUser(res.user);
+				navigate('/app');
+			})
+			.catch((err) => toast.error(err.message));
 
 	return (
 		<div className="bg-base-200 flex h-[100vh] items-center justify-center p-8">
@@ -31,23 +45,29 @@ const Signup = () => {
 				transition={{ duration: 0.5 }}
 				className="bg-base-100 flex aspect-video h-[calc(100%-130px)] max-w-[calc(100%-64px)] rounded-4xl shadow-lg"
 			>
-				<div className="w-full p-10 md:w-1/2">
-					<h1 className="font mb-6 text-3xl">{t('sign-up').capitalize()}</h1>
+				<form onSubmit={handleSubmit(onSubmit)} className="w-full p-10 md:w-1/2">
+					<h1 className="font mb-6 text-3xl flex items-center gap-2">
+						<Link to="/">
+							<Logo />
+						</Link>
+						{t('sign-up').capitalize()}
+					</h1>
 
 					<label htmlFor="email" className="text-sm font-semibold capitalize">
 						{t('email')}
 					</label>
 					<input
 						id="email"
-						value={email}
-						onChange={(e) => setEmail(e.target.value)}
-						onFocus={() => setFieldErrors((prev) => ({ ...prev, email: null }))}
-						className={`input mt-1 placeholder:capitalize ${fieldErrors.email && 'text-error'}`}
+						{...register('email', {
+							required: { value: true, message: 'email-required-msg' },
+							pattern: { value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, message: 'invalid-email-msg' },
+						})}
+						className={`input mt-1 placeholder:capitalize ${errors.email && 'text-error'}`}
 						placeholder={t('email')}
 					/>
-					<div data-hide={!fieldErrors.email} className="text-xs text-error mb-1 mt-0.5 flex items-center gap-1 font-medium h-4 data-[hide=true]:opacity-0">
+					<div data-hide={!errors.email} className="text-xs text-error mb-1 mt-0.5 flex items-center gap-1 font-medium h-4 data-[hide=true]:opacity-0">
 						<CircleAlert size={12} strokeWidth={2.5} />
-						{fieldErrors.email?.capitalize()}
+						{errors.email?.message && t(errors.email?.message).capitalize()}
 					</div>
 
 					<label htmlFor="password" className="text-sm font-semibold capitalize">
@@ -56,22 +76,23 @@ const Signup = () => {
 					<input
 						id="password"
 						type="password"
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-						onFocus={() => setFieldErrors((prev) => ({ ...prev, password: null }))}
-						className={`input mt-1 placeholder:capitalize ${fieldErrors.password && 'text-error'}`}
+						{...register('password', {
+							required: { value: true, message: 'password-required-msg' },
+							pattern: { value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, message: 'not-strong-password-msg' },
+						})}
+						className={`input mt-1 placeholder:capitalize ${errors.password && 'text-error'}`}
 						placeholder={t('password')}
 					/>
-					<div data-hide={!fieldErrors.password} className="text-xs text-error mb-1 mt-0.5 flex items-center gap-1 font-medium h-4 data-[hide=true]:opacity-0">
-						{fieldErrors.password === 'password is not strong enough' ? (
+					<div data-hide={!errors.password} className="text-xs text-error mb-1 mt-0.5 flex items-center gap-1 font-medium h-4 data-[hide=true]:opacity-0">
+						{errors.password?.message === 'not-strong-password-msg' ? (
 							<Tooltip className="gap-1" component="span" content={t('strong-password-message').capitalize()}>
 								<CircleAlert size={12} strokeWidth={2.5} />
-								{fieldErrors.password?.capitalize()}
+								{errors.password?.message && t(errors.password?.message).capitalize()}
 							</Tooltip>
 						) : (
 							<>
 								<CircleAlert size={12} strokeWidth={2.5} />
-								{fieldErrors.password?.capitalize()}
+								{errors.password?.message && t(errors.password?.message).capitalize()}
 							</>
 						)}
 					</div>
@@ -81,36 +102,18 @@ const Signup = () => {
 					</label>
 					<input
 						id="name"
-						value={displayName}
-						onChange={(e) => setDisplayName(e.target.value)}
-						onFocus={() => setFieldErrors((prev) => ({ ...prev, name: null }))}
-						className={`input mt-1 placeholder:capitalize ${fieldErrors.name && 'text-error'}`}
+						{...register('name', { required: { value: true, message: 'name-required-msg' } })}
+						className={`input mt-1 placeholder:capitalize ${errors.name && 'text-error'}`}
 						placeholder={t('display-name')}
 					/>
-					<div data-hide={!fieldErrors.name} className="text-xs text-error mb-1 mt-0.5 flex items-center gap-1 font-medium h-4 data-[hide=true]:opacity-0">
+					<div data-hide={!errors.name} className="text-xs text-error mb-1 mt-0.5 flex items-center gap-1 font-medium h-4 data-[hide=true]:opacity-0">
 						<CircleAlert size={12} strokeWidth={2.5} />
-						{fieldErrors.name?.capitalize()}
+						{errors.name?.message && t(errors.name?.message).capitalize()}
 					</div>
 
 					<button
-						disabled={signingUp}
-						onClick={() => {
-							setSigningUp(true);
-							signup(email, password, displayName)
-								.then((res) => {
-									toast.success(res.message);
-									setUser(res.user);
-									navigate('/app');
-								})
-								.catch((err) => {
-									if (err.statusCode === 422) {
-										setFieldErrors(err.fieldErrors);
-									} else {
-										toast.error(err.message);
-									}
-								})
-								.finally(() => setSigningUp(false));
-						}}
+						disabled={isSubmitting}
+						type="submit"
 						className="from-primary to-secondary mt-6 h-10 w-full rounded-full bg-gradient-to-r font-bold text-neutral-content capitalize hover:shadow-lg"
 					>
 						{t('sign-up')}
@@ -121,7 +124,7 @@ const Signup = () => {
 							{t('sign-in')}
 						</Link>
 					</div>
-				</div>
+				</form>
 				<AbstractGradientShape className="hidden w-1/2 rotate-180 rounded-tl-4xl md:block" />
 			</motion.div>
 		</div>
